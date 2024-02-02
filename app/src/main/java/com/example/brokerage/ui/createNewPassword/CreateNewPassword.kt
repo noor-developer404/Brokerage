70b506b13eccdb5a366967632ed24cf3afa6a9df
noor <noor.developer404@gmail.com>
2024-02-02 18:42:10 +0530

    noor commit 2-2-2024

---
+++ app/src/main/java/com/example/brokerage/ui/createNewPassword/CreateNewPassword.kt
@@ -0,0 +1,12 @@
+package com.example.brokerage.ui.createNewPassword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.brokerage.R
+
+class CreateNewPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_new_password)
+    }
+}
---
+++ app/src/main/java/com/example/brokerage/ui/passwordchanged/PasswordChanged.kt
@@ -0,0 +1,12 @@
+package com.example.brokerage.ui.passwordchanged
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.brokerage.R
+
+class PasswordChanged : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password_changed)
+    }
+}
